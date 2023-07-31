5fa0e180aaa95c8b34ca06b1e128c9e44127bbc5
Ahmadhidayat120402 <ahmadhidayat120402@gmail.com>
2023-07-31 22:53:09 +0700

    bismillah learning dart

---
+++ demo.dart
@@ -0,0 +1,25 @@
+// fungsi pertama kali dijakankan
+void main() {
+  print("hello world");
+
+  // tipe data nama variabele = nilai awal
+  String productName = "Tas Ransel";
+  double productPrice = 15000;
+  int productStoct = 50;
+  bool productAvailable = true;
+
+  print("mengambil data dalam variabel");
+
+  productName = "kopi hitam"; // mengubah nilai variabel;
+
+  print("productName : $productName");
+
+  // konversi tipe data
+  String input = "1000";
+  // string ke doubel
+  double price = double.parse(input);
+  print("price : $price");
+
+  bool isAdmin = true;
+  print('isAdmin : ${isAdmin ? 'ya' : 'tidak'} ');
+}
