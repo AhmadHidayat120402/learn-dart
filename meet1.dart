703ca36e3ca6bd53e4157a64a170c763560e5d67
Ahmadhidayat120402 <ahmadhidayat120402@gmail.com>
2023-08-01 14:51:37 +0700

    code until null safety

--- meet1.dart
+++ meet1.dart
@@ -82,5 +82,127 @@ void main() {
     print("maaf anda tidak mendapatkan diskon");
   }
   // 4. liste and map
+  /*
+  penting terkait list
+    1. mendeklarasikan List
+    2. mendapatkan panjang list
+    3. mengambil nilai dari listindex n
+    4. mengubah nilai dari listindex n
+    5. menambah item baru ke list
+    6. membuang item dari list
+    7. mengosongkan list
+    8. membaca list dengan looping for-item
+   */
+
+  List<String> produucts = ["Odol", "mie instant", "susu kental"];
+
+  // mendapatkan panjang list
+  var lengthList = produucts.length;
+  print("jumlah produk : $lengthList");
+
+  // menambah item baru ke list
+  produucts.add("kopi");
+  print("list produk setelah menamnbah item baru : $produucts");
+  produucts.insert(0, "teh");
+  print(produucts);
+
+  // membuang item list berdasarkan indeex
+  produucts.remove("kopi"); // untuk valuenya
+  print(produucts);
+
+  int removeItem = 1;
+  produucts.removeAt(removeItem); // at untuk index
+  print(produucts);
+
+// memengosongkan list
+  produucts.clear();
+  print("product sekarang : $produucts");
+
+  produucts = [];
+  print(produucts);
+
+  // membaca list dengan looping for in
+  List<String> fruits = ["aple", "pisanag", "jeruk", "lemon"];
+
+  for (var buah in fruits) {
+    var getIndexFruits = fruits.indexOf(buah);
+    print("$getIndexFruits : $buah");
+  }
+
+  // maps
+  /*
+  yang penting di maps
+  1. mendeklarasikan map
+  2. mengmabil nilai dari map dengan key
+  3. mengubah nilai map dengan key
+  4. menambha item dengan key baru ke map
+  5. membuang item dengan key dari map
+
+  */
+  Map<String, int> scores = {"dayat": 90, "riski": 50, "anam": 80};
+
+  // mengambil nilai map dengan key
+  int mymap = scores["dayat"]!;
+  print(mymap);
+
+  // mengubah nilai map dengan key
+  scores["dayat"] = 100;
+  print("map setelah mengubah nilai alice : $scores");
+
+  // menambah item baru ke map
+  scores["ilham"] = 90;
+  print("map setelah menambah item eva : $scores");
+
+  // membuang item dari map
+  scores.remove("ilham");
+  print("seteah map dihapus : $scores");
+
   // 5. null safety
+  String? nama; //ini boleh null atau nullable
+  String nickName = "Dayat";
+
+  int? age;
+  // age = null;
+
+  print("name : $nama");
+  print("age : $age");
+
+  // !null assertion
+  // jika kamu yakin variable message tidka mungkin null
+
+  // maka kamu bisa menggunakan operator null assertion !
+
+  // sendMessage(String message) {
+  //   print(message);
+  // }
+
+  // String? message;
+  // sendMessage(message!);
+  // ini tetap akan error, jika ternyata nilainya null
+
+// ?? null-aware coalescing operator
+// digunakan unutk memberi nilai, jika nilainya null
+  String? Category;
+  String? yourCategory = Category ?? "umum";
+  print(yourCategory);
+  if (Category == null) {
+    print("umum");
+  }
+
+  // ?? null-aware assignment operator
+  // digunakan unutk meberi nilai jika nilai variable nya null
+  String? productNama;
+  productNama ??=
+      "GG filgter 12"; // === productNama = productNama ?? "GG filgter ;
+  print(productNama);
+
+  String myName = "dayat";
+  // myName = null;
+
+  // nulleble || nilainya, mungkin saja null
+  int? usia = 21;
+  // usia = null;
+
+  print("nama : $myName");
+  print("usia : $usia");
 }
